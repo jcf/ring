92267dd3a93b6564147356db79032a789441f294
James Conroy-Finn <james@logi.cl>
2014-12-02 11:38:21 +0000

    Remove trailing whitespace

--- ring-core/test/ring/middleware/test/not_modified.clj
+++ ring-core/test/ring/middleware/test/not_modified.clj
@@ -27,14 +27,14 @@
       (is (= [(handler req) req] ; Not modified since is called with expected args
              ((wrap-not-modified handler) req))))))
 
-(deftest test-not-modified-response  
+(deftest test-not-modified-response
   (testing "etag match"
     (let [known-etag "known-etag"
           request {:headers {"if-none-match" known-etag}}
           handler-resp #(hash-map :status 200 :headers {"etag" %} :body "")]
       (is (= 304 (:status (not-modified-response (handler-resp known-etag) request))))
       (is (= 200 (:status (not-modified-response (handler-resp "unknown-etag") request))))))
-  
+
   (testing "not modified"
     (let [req #(hash-map :headers {"if-modified-since" %})
           last-modified "Sun, 23 Sep 2012 11:00:00 GMT"
@@ -50,7 +50,7 @@
           resp   (not-modified-response h-resp (req last-modified))]
       (is (nil? (:body resp)))
       (is (= (get-in resp [:headers "Content-Length"]) "0"))))
-  
+
   (testing "no modification info"
     (let [response {:status 200 :headers {} :body ""}]
       (is (= 200 (:status (not-modified-response response (etag-request "\"12345\"")))))
